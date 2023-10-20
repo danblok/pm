1f2136db0ad2ef018f9518dc36679957fe21125f
vacatixn <danytokmakov@gmail.com>
2023-10-20 20:08:59 +0500

    feat: add error unwraping to gracefully return correct http statuses

--- internals/handlers/handlers.go
+++ internals/handlers/handlers.go
@@ -1,12 +1,23 @@
 package handlers
 
 import (
+	"errors"
 	"log/slog"
+	"net/http"
 
 	"github.com/danblok/pm/internals/service"
+	"github.com/labstack/echo/v4"
 )
 
 type App struct {
 	Service *service.Service
 	Logger  *slog.Logger
+}
+
+func (a *App) UnwrapError(c echo.Context, logMsg string, err error) error {
+	a.Logger.Error(logMsg, err)
+	if errors.Is(err, service.ErrInternal) {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	return c.NoContent(http.StatusBadRequest)
 }
