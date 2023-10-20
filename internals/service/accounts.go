2842c22eed7085e7e2a81729b9ed8b72afb2fe84
vacatixn <danytokmakov@gmail.com>
2023-10-20 20:29:38 +0500

    fix: make funcs to return custom errros

--- internals/service/accounts.go
+++ internals/service/accounts.go
@@ -22,9 +22,7 @@ type UpdateAccountInput struct {
 	Avatar string `json:"avatar,omitempty"`
 }
 
-// Returns an account by searching with provided id
-//
-// Errors returned: ErrFailedValidation, ErrNoRows
+// Errors returned: ErrFailedValidation, ErrInternal
 func (s *Service) GetAccountById(ctx context.Context, id string) (*types.Account, error) {
 	if _, err := uuid.Parse(id); err != nil {
 		return nil, ErrFailedValidation
@@ -35,26 +33,29 @@ func (s *Service) GetAccountById(ctx context.Context, id string) (*types.Account
 	row := s.DB.QueryRowContext(ctx, query, id)
 	err := row.Scan(&acc.Id, &acc.Email, &acc.Name, &acc.Avatar, &acc.Deleted, &acc.CreatedAt, &acc.UpdatedAt)
 	if err != nil {
-		return nil, err
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNotFound
+		}
+		return nil, ErrInternal
 	}
 
 	return &acc, nil
 }
 
-// Returns a slice accounts by searching with provided id
+// Returne errors: ErrInternal
 func (s *Service) GetAllAccounts(ctx context.Context) ([]types.Account, error) {
 	accs := make([]types.Account, 0)
 
 	rows, err := s.DB.QueryContext(ctx, "SELECT id, email, name, avatar, deleted, created_at, updated_at FROM accounts")
 	if err != nil {
-		return nil, err
+		return nil, ErrInternal
 	}
 
 	for rows.Next() {
 		var acc types.Account
 		err := rows.Scan(&acc.Id, &acc.Email, &acc.Name, &acc.Avatar, &acc.Deleted, &acc.CreatedAt, &acc.UpdatedAt)
 		if err != nil {
-			return nil, err
+			return nil, ErrInternal
 		}
 
 		if !acc.Deleted {
@@ -63,37 +64,29 @@ func (s *Service) GetAllAccounts(ctx context.Context) ([]types.Account, error) {
 	}
 
 	if err = rows.Err(); err != nil {
-		return nil, err
+		return nil, ErrInternal
 	}
 
 	return accs, nil
 }
 
-// Inserts a new record in db
-//
-// Errors returned: ErrFailedValidation, ErrFailedToUpdate
+// Errors returned: ErrFailedValidation, ErrFailedToUpdate, ErrFailedToInsert
 func (s *Service) AddAccount(ctx context.Context, input *AddAccountInput) error {
-	input.validationErrors = make(map[string]string)
-
 	if input.Email == "" {
-		input.validationErrors["email"] = "must be provided"
+		return ErrFailedValidation
 	}
 
 	if input.Name == "" {
-		input.validationErrors["name"] = "must be provided"
-	}
-
-	if len(input.validationErrors) > 0 {
 		return ErrFailedValidation
 	}
 
 	res, err := s.DB.ExecContext(ctx, "INSERT INTO accounts (name, email, avatar) VALUES ($1, $2, $3)", input.Name, input.Email, input.Avatar)
 	if err != nil {
-		return err
+		return ErrInternal
 	}
 	ra, err := res.RowsAffected()
 	if err != nil {
-		return err
+		return ErrFailedToInsert
 	}
 	if ra < 1 {
 		return ErrFailedToUpdate
@@ -102,9 +95,7 @@ func (s *Service) AddAccount(ctx context.Context, input *AddAccountInput) error 
 	return nil
 }
 
-// Updates an account with provided data
-//
-// Errors returned: ErrFailedValidation, ErrFailedToUpdate
+// Errors returned: ErrFailedValidation, ErrFailedToUpdate, ErrInternal
 func (s *Service) UpdateAccount(ctx context.Context, input *UpdateAccountInput) error {
 	if _, err := uuid.Parse(input.Id); err != nil {
 		return ErrFailedValidation
@@ -113,11 +104,11 @@ func (s *Service) UpdateAccount(ctx context.Context, input *UpdateAccountInput) 
 	query := "UPDATE accounts SET name=COALESCE(NULLIF($1, ''), name), email=COALESCE(NULLIF($2, ''), email), avatar=COALESCE(NULLIF($3, ''), avatar) WHERE id=$4"
 	res, err := s.DB.ExecContext(ctx, query, input.Name, input.Email, input.Avatar, input.Id)
 	if err != nil {
-		return err
+		return ErrInternal
 	}
 	ra, err := res.RowsAffected()
 	if err != nil {
-		return err
+		return ErrInternal
 	}
 	if ra < 1 {
 		return ErrFailedToUpdate
@@ -126,20 +117,18 @@ func (s *Service) UpdateAccount(ctx context.Context, input *UpdateAccountInput) 
 	return nil
 }
 
-// Delete an account with provided id
-//
-// Errors returned: ErrFailedValidation, ErrFailedToUpdate
-func (s *Service) DeleteAccount(ctx context.Context, id string) error {
+// Errors returned: ErrFailedValidation, ErrFailedToUpdate, ErrInternal
+func (s *Service) DeleteAccountById(ctx context.Context, id string) error {
 	if _, err := uuid.Parse(id); err != nil {
 		return ErrFailedValidation
 	}
 	res, err := s.DB.ExecContext(ctx, "UPDATE accounts SET deleted=true WHERE id=$1", id)
 	if err != nil {
-		return err
+		return ErrInternal
 	}
 	ra, err := res.RowsAffected()
 	if err != nil {
-		return err
+		return ErrFailedToUpdate
 	}
 	if ra < 1 {
 		return ErrFailedToUpdate
